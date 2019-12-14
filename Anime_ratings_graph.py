d41a86178b33376d74e5d641d94b942efb8ddb18
hiro_mcode <51250939+Mahiro99@users.noreply.github.com>
2019-12-14 18:50:17 -0500

    Fixed labeling issues

--- Anime_ratings_graph.py
+++ Anime_ratings_graph.py
@@ -1,84 +1,37 @@
-from urllib.request import urlopen as request
-from bs4 import BeautifulSoup as bs
-from datetime import date, datetime
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import numpy as np
+import csv
 
-seasonInput = input("Enter Season: ")
-seasonInput=seasonInput.lower()
+ranking_graph=pd.read_csv("seasonalAnimeInfo.csv")
 
-yearInput= input("Enter the Year: ")
+#removing whitespaces
+ranking_graph = ranking_graph.rename(columns={' Studio': 'Studio'})
+ranking_graph = ranking_graph.rename(columns={' Ratings': 'Ratings'})
 
-day = datetime.today().timetuple().tm_yday
+ranking_graph= ranking_graph.sort_values('Ratings', ascending=True)
+#print(ranking_graph.head())
+#print(ranking_graph.describe())
 
-spring = range(80, 172)
-summer = range(172, 264)
-fall = range(264, 355)
-winter=range(355,80)
+x=ranking_graph['Title']
 
+y=ranking_graph.Ratings
+plt.xlabel('Ratings/10')
 
-def findSeason(seasonInput):
-    if day in spring and seasonInput == spring:
-        anime_seasonal_url="https://myanimelist.net/anime/season"
-        return makeItWork(anime_seasonal_url)
+plt.barh(x,y, height=0.5)
 
-    elif day in summer and seasonInput == summer:
-        anime_seasonal_url="https://myanimelist.net/anime/season"
-        return makeItWork(anime_seasonal_url)
-
-    elif day in fall and seasonInput == fall:
-        anime_seasonal_url="https://myanimelist.net/anime/season"
-        return makeItWork(anime_seasonal_url)
-
-    elif day in winter and seasonInput == winter:
-        anime_seasonal_url="https://myanimelist.net/anime/season"
-        return makeItWork(anime_seasonal_url)
-
-    else:
-        anime_seasonal_url="https://myanimelist.net/anime/season/" +yearInput +"/" +seasonInput
-        return makeItWork(anime_seasonal_url)
+#The y-values v are both the x-location and the string values for ax.text, and conveniently the barplot has a metric of 1 for each bar, so the enumeration i is the y-location.
+for i, v in enumerate(y):
+    plt.text(v, i, " "+str(v), color='blue', va='center')
 
 
-def makeItWork(anime_seasonal_url):
-    connection=request(anime_seasonal_url)
-    seasonal_raw_html=connection.read()
-    connection.close()
-
-    parsed_html = bs(seasonal_raw_html,"html.parser")
-
-    main_container=parsed_html.find_all("div", {"class":"seasonal-anime-list js-seasonal-anime-list js-seasonal-anime-list-key-1 clearfix"})
-    newAnimes=main_container[0]
-    newAnime_length=len(main_container[0])
+#y_pos = [i for i, _ in enumerate(x)]
+#plt.barh(x_pos, y, color='red', alpha=0.2)
+#plt.xticks(range(len(x)))
 
 
-    container_animes= parsed_html.findAll("div", {"class":"seasonal-anime js-seasonal-anime"})
-    count=0
+plt.subplots_adjust(left=0.48,bottom=0.07,right=0.99,top=0.95)
+plt.savefig('AnimeData.png', bbox_inches='tight')
+plt.show()
 
-    f=open("seasonalAnimeInfo.csv","w+", encoding="utf-8")
-    header="Title, Studio, Ratings\n"
-    f.write(header)
-
-    for animes in container_animes:
-        if animes in main_container[0]:
-            if count<newAnime_length:
-                numOne = container_animes[count]
-                count+=1
-                title_searcher=numOne.findAll("a",{"class":"link-title"})
-                title_name=title_searcher[0].text.replace(","," ")
-
-                producer_searcher=numOne.find("div",{"class":"prodsrc"})
-                emptyOrNot= producer_searcher.span.text.strip()
-                if emptyOrNot=="-":
-                    studio="Studio not Found"
-                else:
-                    studio=producer_searcher.a["title"]
-
-                rating_searcher=numOne.find("span",{"class":"score"})
-                ratings=rating_searcher.text.strip()
-
-                print(title_name)
-                print(studio)
-                print(ratings)
-                print()
-                f.write(title_name + "," + studio + "," + ratings+"\n")
-    f.close()
-
-findSeason(seasonInput)
